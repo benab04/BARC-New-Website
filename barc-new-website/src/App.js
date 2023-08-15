f2ef1903bdfd17f1af95e9b6e34cdc6e56026689
benab04 <124769045+benab04@users.noreply.github.com>
2023-08-15 19:09:45 +0530

    Add files via upload

---
+++ barc-new-website/src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+import React from "react";
+
+import Hero from "./components/Hero/hero";
+import Navbar from "./components/Navbar/Navbar";
+import AboutUs from "./components/About/AboutUs";
+import WhatWeDo from "./components/WhatWeDo/Wwd";
+import Contact from "./components/Contact/ContactUs";
+import Testimonials from "./components/Testimonials/testimonials";
+import Preloader from "./components/Preloader/Preloader";
+import Gallery from "./components/Gallery/gallery";
+// import Team from "./components/Team/Teams";
+import ReactDOM from "react-dom/client";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+function App(){
+  return (
+  <div>  
+  <Preloader />
+  <Navbar />
+  <Hero />
+  <AboutUs />
+  <WhatWeDo />
+  <Testimonials />
+  <Gallery /> 
+   {/* <Team />  */}
+   <Contact /> 
+  </div>
+  );
+}
+
+export default App;
